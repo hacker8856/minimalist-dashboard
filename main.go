5d644842edf35ef348f6ba7aa49aa27c638a7cdd
bastienlegall <bastrofort17@gmail.com>
2025-09-24 18:00:52 +0200

    added zfs architecture

--- main.go
+++ main.go
@@ -279,7 +279,6 @@ func getZFSConfig() ZFSConfig {
 	var dataVdevs []ZPoolVdev
 	var cacheVdev *ZPoolVdev
 	
-	// On lit les infos simples en haut du fichier
 	for _, line := range strings.Split(out, "\n") {
 		fields := strings.Fields(line)
 		if len(fields) < 2 { continue }
@@ -295,16 +294,17 @@ func getZFSConfig() ZFSConfig {
 	var lastVdev *ZPoolVdev
 
 	for _, line := range strings.Split(out, "\n") {
-		// On ne commence le parsing qu'après l'en-tête de la configuration
 		if strings.Contains(line, "NAME") && strings.Contains(line, "STATE") {
 			inConfigSection = true
 			continue
 		}
+		// NOUVELLE CONDITION : On arrête tout si on arrive à la section des erreurs
+		if strings.HasPrefix(line, "errors:") {
+			break
+		}
 		if !inConfigSection || len(strings.TrimSpace(line)) == 0 {
 			continue
 		}
-
-		// On ignore la ligne qui contient le nom du pool, car on l'a déjà
 		if strings.HasPrefix(strings.TrimSpace(line), config.PoolName) {
 			continue
 		}
@@ -315,17 +315,13 @@ func getZFSConfig() ZFSConfig {
 		deviceName := fields[0]
 		deviceStatus := fields[1]
 
-		// On identifie le rôle de la ligne par son nom
 		if strings.Contains(deviceName, "raidz") || strings.Contains(deviceName, "mirror") {
-			// C'est un nouveau vdev de données
 			dataVdevs = append(dataVdevs, ZPoolVdev{Name: deviceName, Status: deviceStatus})
-			lastVdev = &dataVdevs[len(dataVdevs)-1] // On pointe sur le dernier vdev ajouté
+			lastVdev = &dataVdevs[len(dataVdevs)-1]
 		} else if deviceName == "cache" {
-			// C'est le vdev de cache
 			cacheVdev = &ZPoolVdev{Name: deviceName, Status: deviceStatus}
 			lastVdev = cacheVdev
 		} else if lastVdev != nil {
-			// Si ce n'est pas un vdev, c'est un disque appartenant au dernier vdev trouvé
 			lastVdev.Devices = append(lastVdev.Devices, deviceName)
 		}
 	}
